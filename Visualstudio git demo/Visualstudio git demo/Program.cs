d89e9cdb13d15ead9702426a3515a8ec1f45855b
Cassandra <cassiiee93@outlook.com>
2023-10-11 11:42:38 +0200

    First commit

---
+++ Visualstudio git demo/Visualstudio git demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Visualstudio_git_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hejsan Världen");
+        }
+    }
+}
